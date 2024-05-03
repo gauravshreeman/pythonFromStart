174499b193736187d995723b384f9df46e73e591
Gaurav Kumar Tiwari <gauravkrtiwari108@gmail.com>
2024-05-03 23:56:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+print("Om Ganeshay Namah" " " 'Jay Shree Ram')
+
+#variable. It is a container.
+#string
+first_name = "Gaurav Kumar Tiwari"
+last_name = "Doctor"
+print (first_name +" "+ last_name)
+
+#integer
+age = 21
+age += 1
+print("Your age is " + str(age))
+print (type(age))
+
+#float. Used to store decimal number 
+height = 281.27  
+print ("your height is " + str(height) + "cm")
+print (type(height))
+
+#boolean
+human = True
+print ("Are you a Human?" + str(human))
+print (type(human))
+
+#multiple assignment
+a, b = 1, 2
+print(a, b)
+name, age, attractive = "Gaurav", 21, True
+print(name, age, attractive)
+Gaurav = Roshan = Param = Sarwan = 21
+print (Gaurav, Roshan, Param, Sarwan)
